91ead2dc0f29e7d1885f335f8b02e4800ece98bd
Don Livingston <donlivingston@gmail.com>
2024-01-08 14:50:47 -0500

    Quia Ego Sic Dico

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-// Get the HTML content from the provided link
 
-const { JSDOM } = require('jsdom');
-const url = 'https://tns4lpgmziiypnxxzel5ss5nyu0nftol.lambda-url.us-east-1.on.aws/challenge';
-
-const getFlag = async () => {
-  try {
-    const response = await fetch(url);
-    const html = await response.text();
-    const dom = new JSDOM(html);
-    const urlCharacters = [];
-
-    const elements = dom.window.document.querySelectorAll('[data-class^="23"]>[data-tag$="93"]>[data-id*="21"]>i');  
-    elements.forEach(element => {
-      urlCharacters.push(element.getAttribute('value'));
-    });
-    const flag = urlCharacters.join('');
-    console.log(flag);
-
-  } catch (error) {
-    console.log(error);
+function multiplyArrayElements(array) {
+  let result = 1;
+  for (let num of array) {
+    result = result * num;
   }
+  return result;
 }
 
-getFlag();
+console.log(multiplyArrayElements([2, 3, 5, 7])); 
+// 210
